50a88307ef288e41bd7166dd2482d17df6bcccb7
stephenmssuresh <stephenmssuresh@gmail.com>
2023-01-15 18:01:04 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,93 @@
+// Setup
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MoviesDB = require("./modules/moviesDB.js");
+const db = new MoviesDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+const HTTP_PORT = process.env.PORT || 8080;
+// Or use some other port number that you like better
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API Listening' });
+});
+
+//This route uses the body of the request to add a new "Movie" document to the collection and return the newly created movie object / fail message to the client.
+app.post('/api/movies', (req, res) => {
+    if (!req.body) {
+        res.status(204).json({ message: 'Nothing in req.body' });
+    }
+    else () => {
+        db.addNewMovie(req.body)
+            .then((data) => {
+                res.status(201).json(data);
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    }
+})
+
+app.get("/api/movies", (req, res) => {
+    if (!req.query || !req.query.page || !req.query.perPage) {
+        res.status(500).json({ message: "Invalid query/parameter entered" });
+    } else {
+        db.getAllMovies(req.query.page, req.query.perPage, req.query.title)
+            .then(
+                (data) => {
+                    res.status(201).json(data);
+                })
+            .catch((err) => {
+                // res.status(500).json({message : err});
+                res.status(500).json(err);
+            });
+    }
+});
+
+app.get('api/movies/:id', (req, res) => {
+    db.getMovieById(req.params.id)
+        .then((data) => {
+            res.status(201).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+})
+
+app.put('/api/movie/:id', (req, res) => {
+    db.updateMovieById(req.body, req.params.id)
+        .then((data) => {
+            res.status(201).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+})
+
+app.delete('/api/movies/:id', (req, res) => {
+    db.deleteMovieById(req.params.id)
+        .then((data) => {
+            res.status(201).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
+})
+
+db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log(`server listening on: ${HTTP_PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
