cd2436e230aaff8ab2d255108640df61095c772b
Senecassuresh15 <ssuresh15@myseneca.ca>
2023-04-01 14:05:56 -0400

    commit to fix cyclic

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Stephen Suresh         Student ID: 117916213      Date: 2023-01-15
+*  Cyclic Link: https://modern-calf-vest.cyclic.app/
+*
+********************************************************************************/ 
+
+
 // Setup
 require('dotenv').config();
 
